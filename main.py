9677834665dd42e84f7ef3d9c46d7f6165c59ba6
lgltan <lgltan20@gmail.com>
2023-11-13 20:45:50 +0800

    added stack and queue

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+# Tan, Lance Griffin L.
+# CSC615M - Machine Project
+
 import customtkinter
 
-def SCAN():
-    return
+class STACK:
+    def __init__(self, stack_name):
+        self.stack_name = stack_name
+        self.stack = []
+    
+    def READ(self):
+        return self.stack.pop()
+
+    def WRITE(self, write_val):
+        self.stack.append(write_val)
+
+    def SCAN(self):
+        return self.stack[-1]
+
+class QUEUE:
+    def __init__(self, queue_name):
+        self.queue_name = queue_name
+        self.queue = []
+    
+    def READ(self):
+        return self.queue.pop(0)
+
+    def WRITE(self, write_val):
+        self.queue.append(write_val)
+
+    def SCAN(self):
+        return self.queue[0]
 
 def SCAN_LEFT():
     return
 
 def SCAN_RIGHT():
-    return
-
-def READ():
-    return
-
-def WRITE():
     return
 
 def TAPE_RIGHT():
@@ -30,11 +52,47 @@ def TAPE_UP():
 def TAPE_DOWN():
     return
 
-def print_here():
-    print("hello")
+def process_input(input_data):
+    section_split = input_data.split(".")
+
+    while "" in section_split:
+        section_split.remove("")
+
+    if "DATA" in section_split[0]:
+        data_arr = section_split[0]
+    elif "DATA" in section_split[1]:
+        data_arr = section_split[1]
+    else:
+        print("WARNING: Could not find .DATA section.")
+
+    if "LOGIC" in section_split[0]:
+        logic_arr = section_split[0]
+    elif "LOGIC" in section_split[1]:
+        logic_arr = section_split[1]
+    else:
+        print("WARNING: Could not find .LOGIC section.")
+
+    data_arr = data_arr.replace("DATA", "").split("\n")
+    logic_arr = logic_arr.replace("LOGIC", "").split("\n")
+
+    while "" in data_arr:
+        data_arr.remove("")
+
+    while "" in logic_arr:
+        logic_arr.remove("")
+
+    if len(data_arr) == 0:
+        print("WARNING: No .DATA given")
+    if len(logic_arr) == 0:
+        print("WARNING: No .LOGIC given")
+
+    return data_arr, logic_arr
 
 def run():
-    print_here()
+    data_arr, logic_arr = process_input(tb_input.get("1.0",'end-1c'))
+    
+    print(f"Data Arr: {data_arr}")
+    print(f"Logic Arr: {logic_arr}")
     return
 
 # ---------------------------------------------------------
@@ -43,26 +101,21 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("1920x1080")
+root.geometry("1280x720")
 root.title("CSC615M - Machine Project")
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 label = customtkinter.CTkLabel(master=frame, text="CSC615M: Machine Project - Tan, L.", font=("Roboto", 32))
-label.pack(pady=12, padx=10)
+label.pack(pady=40)
 
-label_data = customtkinter.CTkLabel(master=frame, text="Data", font=("Roboto", 24))
-label_data.pack(pady=2, padx=10)
-tb_data = customtkinter.CTkTextbox(master=frame, width=600, height=160)
-tb_data.pack(pady=40, padx=80)
-
-label_logic = customtkinter.CTkLabel(master=frame, text="Logic", font=("Roboto", 24))
-label_logic.pack(pady=2, padx=10)
-tb_logic = customtkinter.CTkTextbox(master=frame, width=600, height=160)
-tb_logic.pack(pady=40, padx=80)
+label_input = customtkinter.CTkLabel(master=frame, text="Input Section", font=("Roboto", 24))
+label_input.pack(pady=2)
+tb_input = customtkinter.CTkTextbox(master=frame, width=600, height=160)
+tb_input.pack(pady=10)
 
 button = customtkinter.CTkButton(master=frame, text="Run", command=run)
-button.pack(pady=40, padx=80)
+button.pack(pady=10)
 
 root.mainloop()
